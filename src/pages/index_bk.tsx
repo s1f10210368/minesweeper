b058d348311b7a315487e76313789a3cdc440f52
s1f10210368 <s1f102103684@iniad.org>
2024-01-25 17:25:56 +0900

    complete

--- src/pages/index_bk.tsx
+++ src/pages/index_bk.tsx
@@ -131,7 +131,7 @@ const Home = () => {
     // ユーザー入力にボムの数を設定
     setUserInput((prev) => {
       const newInput = [...prev];
-      newInput[y][x] = count + 1; // countが0の場合は空のセルを意味する //修正必要
+      newInput[y][x] = (count + 1) as 0 | 1 | 2 | 3 | 11;
       return newInput;
     });
   };
